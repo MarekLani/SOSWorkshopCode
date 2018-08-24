e6f5990cad3c2c2afff83bdb1ffe014dbe65cd79
Marek <mareklani@gmail.com>
2018-08-24 17:49:11 +0200

    Initial Commit

---
+++ AlertProcessing/AlertProcessing/AlertProcessingFunction.cs
@@ -0,0 +1,39 @@
+using Microsoft.Azure.Devices;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Azure.WebJobs.ServiceBus;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Threading.Tasks;
+
+namespace AlertProcessing
+{
+    public static class AlertProcessingFunction
+    {
+        [FunctionName("AlertProcessingFunction")]
+        public static async Task Run([EventHubTrigger("alerteh", Connection = "EventHubConnectionString")]string myEventHubMessage, ILogger log)
+        {
+            try
+            {
+                var s_serviceClient = ServiceClient.CreateFromConnectionString(System.Environment.GetEnvironmentVariable(
+                    "IoTHubConnectionString", EnvironmentVariableTarget.Process));
+
+                dynamic jsonObject = JsonConvert.DeserializeObject(myEventHubMessage);
+
+                var methodInvocation = new CloudToDeviceMethod("StartAlarm") { ResponseTimeout = TimeSpan.FromSeconds(30) };
+                methodInvocation.SetPayloadJson(@"{""CO2"":""" + (string)jsonObject.avgco2 + @"""}");
+                //methodInvocation.SetPayloadJson("10");
+                // Invoke the direct method asynchronously and get the response from the simulated device.
+                var response = await s_serviceClient.InvokeDeviceMethodAsync((string)jsonObject.deviceid, methodInvocation);
+
+                log.LogInformation("Response status: {0}, payload:", response.Status);
+                log.LogInformation(response.GetPayloadAsJson());
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+    }
+}
---
+++ SensorDataSenderApp/SensorDataSender/Program.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Azure.Devices.Client;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileReaderApp
+{
+    class Program
+    {
+        private static IConfigurationRoot configuration;
+
+        static async Task Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appSettings.json", optional: true, reloadOnChange: true);
+
+            configuration = builder.Build();
+
+            var s_deviceClient = DeviceClient.CreateFromConnectionString(configuration.GetConnectionString("IoTHubConnectionString"), Microsoft.Azure.Devices.Client.TransportType.Mqtt);
+
+            while (true)
+            {
+                await SendSimulatedSensorData(s_deviceClient);
+                await Task.Delay(10000);
+            }
+        }
+
+        private static async Task SendSimulatedSensorData(DeviceClient deviceClient)
+        {
+            SensorData sd = new SensorData();
+            sd.TimeStamp = DateTime.Now;
+            Random r = new Random();
+            sd.CO2 = r.NextDouble()*100;
+            sd.Temp = r.NextDouble() * 100;
+
+            var message = JsonConvert.SerializeObject(sd);
+
+            Console.WriteLine(message);
+            await deviceClient.SendEventAsync(new Message(Encoding.ASCII.GetBytes(message)));
+        }
+    }
+
+    public class SensorData
+    {
+        public DateTime TimeStamp { get; set; }
+        public double CO2 { get; set; }
+
+        public double Temp { get; set; }
+        
+    }
+}
