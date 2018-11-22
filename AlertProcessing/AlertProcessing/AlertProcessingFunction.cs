5f75f597d6aa85acb06988adf9ed44271854e85d
Marek <mareklani@gmail.com>
2018-11-22 19:18:55 +0100

    v 2.0 breaking changes

--- AlertProcessing/AlertProcessing/AlertProcessingFunction.cs
+++ AlertProcessing/AlertProcessing/AlertProcessingFunction.cs
@@ -1,7 +1,5 @@
 using Microsoft.Azure.Devices;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
-using Microsoft.Azure.WebJobs.ServiceBus;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using System;
