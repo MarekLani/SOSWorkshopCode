1c81932fd655793659221f08595cd158acf9db1f
Marek <mareklani@gmail.com>
2018-11-27 20:02:59 +0100

    Reset

--- FileReaderApp/FileReaderApp/Program.cs
+++ FileReaderApp/FileReaderApp/Program.cs
@@ -12,21 +12,6 @@ namespace FileReaderApp
 {
     class Program
     {
-        public UInt32 nPinCount = 0;
-        public static int groupSelected = 1;
-        public UInt32 bDioDisable = 0;
-
-        [DllImport("aaeonEAPI.dll", EntryPoint = "EApiGPIOSetDirection")]
-        public static extern UInt32 EApiGPIOSetDirection(UInt32 Id, UInt32 Bitmask, UInt32 Direction);
-        
-        [DllImport("aaeonEAPI.dll", EntryPoint = "EApiGPIOSetLevel")]
-        public static extern UInt32 EApiGPIOSetLevel(UInt32 Id, UInt32 Bitmask, UInt32 Level);
-       
-        [DllImport("aaeonEAPI.dll", EntryPoint = "EApiLibInitialize")]
-        public static extern UInt32 EApiLibInitialize();
-
-        [DllImport("aaeonEAPI.dll", EntryPoint = "EApiLibUnInitialize")]
-        public static extern UInt32 EApiLibUnInitialize();
 
         private static IConfigurationRoot configuration;
 
@@ -40,16 +25,6 @@ namespace FileReaderApp
 
             var s_deviceClient = DeviceClient.CreateFromConnectionString(configuration.GetConnectionString("IoTHubConnectionString"), Microsoft.Azure.Devices.Client.TransportType.Mqtt);
 
-            //Initialize DIO
-            UInt32 err = EApiLibInitialize();
-            if (err != EAPI.EAPI_STATUS_INITIALIZED)
-            {
-                Console.WriteLine("Can't initialize the application: " + err.ToString("X"));
-            }
-
-
-            // Create a handler for the direct method call
-            s_deviceClient.SetMethodHandlerAsync("StartAlarm", StartAlarm, null).Wait();
 
             while (true)
             {
@@ -74,130 +49,6 @@ namespace FileReaderApp
 
             Console.WriteLine(message);
             await deviceClient.SendEventAsync(new Message(Encoding.ASCII.GetBytes(message)));
-        }
-
-        private static async Task SendSensorData(DeviceClient deviceClient)
-        {
-            string path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
-
-            Console.WriteLine(path);
-            long oldLength = 0;
-
-            var directory = new DirectoryInfo(configuration["SensorDataPath"]);
-
-            //We will be reading from the most fresh files with sensor data
-            var shtFile = (from f in directory.GetFiles()
-                           where f.Name.Contains("SHT")
-                           orderby f.CreationTime descending
-                          select f).First();
-
-            var sgpFile = (from f in directory.GetFiles()
-                           where f.Name.Contains("SGP")
-                           orderby f.CreationTime descending
-                           select f).First();
-
-            Console.WriteLine(shtFile.FullName);
-            Console.WriteLine(sgpFile.FullName);
-            DateTime lastSHTMeasurementTime = DateTime.MinValue;
-            DateTime lastSGPMeasurementTime = DateTime.MinValue;
-
-            SensorData sd = new SensorData();
-
-            while (true)
-            {
-                bool newRecordsRead = false;
-
-                var lastLine = ReadLastLine(shtFile.FullName);
-                var sensorDataArray = lastLine.Split('\t');
-
-                var lineTime = UnixTimeStampToDateTime(Convert.ToDouble(sensorDataArray[0]));
-
-                //is there new data to read from SHT sensor
-                if (lastSHTMeasurementTime < lineTime) {
-                    if (lineTime > sd.TimeStamp)
-                        sd.TimeStamp = lineTime;
-
-                    lastSHTMeasurementTime = lineTime;
-                    sd.Temp = Convert.ToDouble(sensorDataArray[1]);
-                    newRecordsRead = true;
-                }
-
-                lastLine = ReadLastLine(sgpFile.FullName);
-
-                sensorDataArray = lastLine.Split('\t');
-
-                lineTime = UnixTimeStampToDateTime(Convert.ToDouble(sensorDataArray[0]));
-
-                //is there new data to read from SGP sensor
-                if (lastSGPMeasurementTime < lineTime)
-                {
-                    if(lineTime > sd.TimeStamp)
-                        sd.TimeStamp = lineTime;
-
-                    lastSGPMeasurementTime = lineTime;
-                    sd.CO2 = Convert.ToDouble(sensorDataArray[1]);
-                    newRecordsRead = true;
-                }
-
-                if (newRecordsRead){
-                    var message = JsonConvert.SerializeObject(sd);
-                    Console.WriteLine(message);
-                    await deviceClient.SendEventAsync(new Message(Encoding.ASCII.GetBytes(message)));
-                }
-                else
-                {
-                    await Task.Delay(5000);
-                }
-            }
-        }
-
-        private static Task<MethodResponse> StartAlarm(MethodRequest methodRequest, object userContext)
-        {
-            dynamic jsonObject = JsonConvert.DeserializeObject(methodRequest.DataAsJson);
-            Console.WriteLine("ALARM CO2 :"+ (string)jsonObject.CO2);
-
-            // Initiate alar for 5 seconds
-            Task.Run(async () =>
-            {
-                SetDioPinState(2, 0, 1);
-                Console.WriteLine("Alarm START");
-                await Task.Delay(5000);
-                Console.WriteLine("Alarm STOP");
-                SetDioPinState(2, 0, 0);
-
-            });
-
-
-            //respond to IoT Hub
-            string result = "{\"result\":\"Executed direct method: " + methodRequest.Name + "\"}";
-            return Task.FromResult(new MethodResponse(Encoding.UTF8.GetBytes(result), 200));
-        }
-
-        /// <summary>
-        /// Sets value on GPIO PIN
-        /// </summary>
-        /// <param name="dPin">number of pin</param>
-        /// <param name="nInput">iput/output  1/0</param>
-        /// <param name="nHigh">on/off 1/0</param>
-        public static void SetDioPinState(UInt32 dPin, UInt32 nInput, UInt32 nHigh)
-        {
-            UInt32 err1 = EAPI.EAPI_STATUS_SUCCESS;
-            UInt32 err2 = EAPI.EAPI_STATUS_SUCCESS;
-            
-            err1 = EApiGPIOSetDirection(EAPI.EAPI_GPIO_GPIO_ID((UInt32)(dPin + (8 * (groupSelected - 1)))), 0xFFFFFFFF, nInput);
-            err2 = EApiGPIOSetLevel(EAPI.EAPI_GPIO_GPIO_ID((UInt32)(dPin + (8 * (groupSelected - 1)))), 0xFFFFFFFF, nHigh);
-
-            if (err1 != EAPI.EAPI_STATUS_SUCCESS || err2 != EAPI.EAPI_STATUS_SUCCESS)
-            {
-                if (err1 == EAPI.EAPI_STATUS_DEVICE_NOT_READY || err2 == EAPI.EAPI_STATUS_DEVICE_NOT_READY)
-                {
-                    Console.WriteLine( "Can't set DIO" + (dPin + 1 + (8 * (groupSelected - 1))).ToString() + " value:\nHardware not ready. Please check BIOS setting.");
-                }
-                else
-                {
-                    Console.WriteLine("Can't set DIO value.");
-                }
-            }
         }
 
         public static String ReadLastLine(string path)
